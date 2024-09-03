12cddc8416c2bda7a1fde8192d6276a0c9133242
Corno Schraverus <corno@schraverus.com>
2024-09-03 15:51:26 +0200

    .

--- build/scripts/publish.sh
+++ build/scripts/publish.sh
@@ -9,20 +9,30 @@ if [ -z "$1" ]
     exit 1
 fi
 
-#the scriptsdir will be deleted, change to the root
-cd "$rootDir" && \
-
 #make sure everything is pushed
 git push && \
 
 #validate that everything is committed and pushed (to make sure we're not messing with open work)
 git diff --exit-code && git log origin/master..master --exit-code && \
 
-./build.sh && \
+node $scriptDir/../node_modules/npm-updatedependencies2latest/dist/index.js $rootDir/pub dependencies && \
+
+$scriptDir/build.sh && \
 
 #validate that everything is still committed after the update and build
 git diff --exit-code && git log origin/master..master --exit-code && \
 
 pushd "$rootDir/pub" > /dev/null && \
-npm version $1 && \
+
+#bump version and store in variable
+newVersion=$(npm version "$1") && \
+
+#commit package.json with new version number
+git add --all && \
+git commit -m "version bumped to $newVersion" && \
+
+#create a tag
+git tag -a "$newVersion" -m "$newVersion" && \
+
+git push && \
 npm publish
